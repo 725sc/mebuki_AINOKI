bb78abf8004f0a2700e6b658dedcd2269c78044e
725sc <53557182+725sc@users.noreply.github.com>
2022-05-11 20:07:45 +0900

    Add files via upload

---
+++ js:common.js
@@ -0,0 +1,38 @@
+if ($("#slider1").length > 0) {
+  swiper1 = new Swiper("#slider1", {
+    navigation: {
+      nextEl: "#swiper-button-next1",
+      prevEl: "#swiper-button-prev1",
+    },
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+  swiper2 = new Swiper("#slider2", {
+    navigation: {
+      nextEl: "#swiper-button-next2",
+      prevEl: "#swiper-button-prev2",
+    },
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+  swiper3 = new Swiper("#slider3", {
+    navigation: {
+      nextEl: "#swiper-button-next3",
+      prevEl: "#swiper-button-prev3",
+    },
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+}
